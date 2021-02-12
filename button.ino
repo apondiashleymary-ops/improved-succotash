c38d9fd53cd8e1836368e980c427477a5bff5c11
ApondiAshley <57188573+apondiashleymary-ops@users.noreply.github.com>
2021-02-13 02:44:08 +0300

    Add files via upload
    Coding with ESP32 using Arduino IDE controlling LED with a push button

---
+++ button.ino
@@ -0,0 +1,30 @@
+ int ledPin=19;  //led pin
+ int buttonPin=22;   //pushbutton is connected
+ int buttonstate=1;   //varibale for reading button status
+ 
+//
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ledPin,OUTPUT);    //declare led as output
+   pinMode( buttonPin,INPUT);  //declare button as input
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   buttonstate=digitalRead( buttonPin);
+  if( buttonstate==HIGH)//
+  {
+   digitalWrite(ledPin,HIGH);
+   Serial.println("Led is On");
+  
+
+  }
+  else
+  {
+    digitalWrite(ledPin,LOW);
+       Serial.println("Led is Off");
+         
+  }
+}
